86799056bfc2c2728d2ab9104e6e520b23e9c626
Ramy <ramyfarid922@gmail.com>
2021-10-16 20:21:20 +0200

    Trying filesystem read write

--- main.c
+++ main.c
@@ -1,10 +1,19 @@
+#include <assert.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <string.h>
 #include <uv.h>
 
-/* global counter defined */
-int64_t counter = 0;
+void on_read(uv_fs_t *req);
+
+uv_fs_t open_req;
+uv_fs_t read_req;
+uv_fs_t write_req;
+
+static char buffer[1024];
+
+static uv_buf_t iov;
 
 void runCPlayground()
 {
@@ -34,29 +43,63 @@ void runCPlayground()
     struct Employee e3 = {3, 5, "Bill", 1900.50};
 }
 
-void wait_for_a_while(uv_idle_t *handle)
+void on_write(uv_fs_t *req)
 {
-    // This function is a callback to an event, in the libuv C world
-    // It just increments a global counter on each invokation
-    counter++;
+    if (req->result < 0)
+    {
+        fprintf(stderr, "Write error: %s\n", uv_strerror((int)req->result));
+    }
+    else
+    {
+        uv_fs_read(uv_default_loop(), &read_req, open_req.result, &iov, 1, -1, on_read);
+    }
+}
 
-    // If this function ran 10e6
-    // Just remove the registered idle handle
-    if (counter >= 10e6)
-        uv_idle_stop(handle);
+void on_read(uv_fs_t *req)
+{
+    if (req->result < 0)
+    {
+        fprintf(stderr, "Read error: %s\n", uv_strerror(req->result));
+    }
+    else if (req->result == 0)
+    {
+        uv_fs_t close_req;
+        // synchronous
+        uv_fs_close(uv_default_loop(), &close_req, open_req.result, NULL);
+    }
+    else if (req->result > 0)
+    {
+        iov.len = req->result;
+        uv_fs_write(uv_default_loop(), &write_req, 1, &iov, 1, -1, on_write);
+    }
+}
+
+void on_open(uv_fs_t *req)
+{
+    // The request passed to the callback is the same as the one the call setup
+    // function was passed.
+    assert(req == &open_req);
+    if (req->result >= 0)
+    {
+        iov = uv_buf_init(buffer, sizeof(buffer));
+        uv_fs_read(uv_default_loop(), &read_req, req->result,
+                   &iov, 1, -1, on_read);
+    }
+    else
+    {
+        fprintf(stderr, "error opening file: %s\n", uv_strerror((int)req->result));
+    }
 }
 
 int main(int argc, char **argv)
 {
-    uv_idle_t idler;
-
-    /* Registering an idle uv handle */
-    uv_idle_init(uv_default_loop(), &idler);
-    uv_idle_start(&idler, wait_for_a_while);
-
-    printf("Idling...\n");
+    // The path of the file to be opened is supplied via the cli arguments to running the executable
+    // The path is captured in argv[1]
+    uv_fs_open(uv_default_loop(), &open_req, argv[1], O_RDONLY, 0, on_open);
     uv_run(uv_default_loop(), UV_RUN_DEFAULT);
 
-    uv_loop_close(uv_default_loop());
+    uv_fs_req_cleanup(&open_req);
+    uv_fs_req_cleanup(&read_req);
+    uv_fs_req_cleanup(&write_req);
     return 0;
 }
